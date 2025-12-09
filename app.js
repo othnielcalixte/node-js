e17c52cfbcea67bba7e40be081b324a3a31e1495
othnielcalixte <othnielcalixte@gmail.com>
2025-12-08 23:12:37 -0500

    Implementing MongoDB in the project - start

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -18,77 +12,26 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
 });
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' }); //this is saying that user created this product, we are not talking about purchasing yet.
-User.hasMany(Product); // meaning a user can add more than one item to the shop
-User.hasOne(Cart); // you only need one of these, bot both but it is good to know
-Cart.belongsTo(User); // this will add a new field to cart which is the user id to which the cart belongs
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-    // console.log(result);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: 'Max', email: 'test@test.com' });
-    }
-    return user;
-  })
-  .then(user => {
-    // console.log(user);
-    return user.createCart();
-  })
-  .then(cart => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  }); //this syncs your models to the db by creating the appropriate tables and relations
-
-// Note:
-// seeting force to true overides you entire table see below:
-/* sequelize
-  .sync({ force: true })
-  .then(result => {
-    // console.log(result);
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });*/
-//You can use then here because we are using a pool that has promises in the database.js file
-// example code below
-// db.execute('SELECT * FROM products')
-//   .then(result => {
-//     console.log(result[0], result[1]);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-//remember you need to move about the app.listen line to use it
+mongoConnect(client => {
+  app.listen(3000);
+});
